9e4f880e4857267740e2b335800d19b2589c6c9b
Victor Boleac <victor.boleac@icloud.com>
2024-12-19 11:52:33 +0000

    feat: updated onCreate method from UpdateAlbumActivity class

--- app/src/main/java/com/example/recordshop/ui/updatealbum/UpdateAlbumActivity.java
+++ app/src/main/java/com/example/recordshop/ui/updatealbum/UpdateAlbumActivity.java
@@ -7,9 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.recordshop.R;
 import com.example.recordshop.databinding.ActivityUpdateAlbumBinding;
+import com.example.recordshop.model.Album;
+import com.example.recordshop.ui.mainactivity.MainActivityViewModel;
 
 public class UpdateAlbumActivity extends AppCompatActivity {
 
@@ -17,12 +21,25 @@ public class UpdateAlbumActivity extends AppCompatActivity {
     private ActivityUpdateAlbumBinding binding;
 
     private UpdateAlbumClickHandler handler;
+    private Album album;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_update_album);
+
+        album = getIntent().getParcelableExtra(ALBUM_KEY, Album.class);
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_update_album
+        );
+        MainActivityViewModel viewModel = new ViewModelProvider(this)
+                .get(MainActivityViewModel.class);
+        handler = new UpdateAlbumClickHandler(album, this, viewModel);
+        binding.setAlbum(album);
+        binding.setClickHandler(handler);
 
     }
 }
