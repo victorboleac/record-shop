27e06c362ea00a449798388212ef800b4da05c03
Victor Boleac <victor.boleac@icloud.com>
2025-01-07 09:06:15 +0000

    adding fragments icons

---
+++ app/src/main/java/com/example/recordshop/navigation/fragments/AddFragment.java
@@ -0,0 +1,28 @@
+package com.example.recordshop.navigation.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.recordshop.R;
+
+public class AddFragment extends Fragment {
+
+
+    public AddFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+}
